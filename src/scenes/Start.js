ab68b4cf69d44290c16a505bf6e3af8ce9b7cab7
nLuck <60993438+nLuck@users.noreply.github.com>
2025-10-16 18:08:49 -0700

    starting
    cloned repo and forked it

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -6,18 +6,26 @@ export class Start extends Phaser.Scene {
 
     preload() {
         this.load.image('background', 'assets/bg.png');
+        this.load.image('player_sprite', 'assets/playerShip2_blue.png');
 
     }
 
     create() {
         this.background = this.add.sprite(640, 320, 'background');
+        this.player = this.add.sprite(640, 320, 'player');
         this.last_time = 0;
+
+        this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     }
 
-    update(time) {
+    update(time, dt) {
         let dt = (time - this.last_time)/1000;
         this.last_time = time;
         
+        if (this.leftKey.isDown) {
+            
+        }
+
     }
     
 }
