73928ad3c956e873e732a1e6c27de017145de7ef
nLuck <60993438+nLuck@users.noreply.github.com>
2025-10-16 19:12:06 -0700

    movement
    left and right

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -16,14 +16,21 @@ export class Start extends Phaser.Scene {
         this.last_time = 0;
 
         this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
     }
 
-    update(time, dt) {
+    update(time) {
         let dt = (time - this.last_time)/1000;
         this.last_time = time;
+
+        let speed = 50;
         
         if (this.leftKey.isDown) {
-            
+            this.player.x -= speed * dt;
+        }
+        if (this.rightKey.isDown) {
+            this.player.x += speed * dt;
         }
 
     }
